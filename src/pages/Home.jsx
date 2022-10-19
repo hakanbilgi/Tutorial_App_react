570bebbe30b185368d08efb43195e4f8058c8551
Hakan Bilgi <hknblg34@gmail.com>
2022-10-19 16:08:21 +0300

    axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
+import axios from "axios";
 
 const Home = () => {
+
+
+
   return (
     <>
       <AddTutorial />
