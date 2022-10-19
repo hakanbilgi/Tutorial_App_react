686bc5bc26f1bf48176ea09715f32de8df36f4b0
Hakan Bilgi <hknblg34@gmail.com>
2022-10-19 21:20:51 +0300

    axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,28 @@
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 const Home = () => {
- const [tutorials,setTutorials] = useState ()
+ const [tutorials,setTutorials] = useState ([])
 
 const url = "https://tutorials-api-cw.herokuapp.com/api/tutorials";
 
 const getTutorials = async ()=>{
   
-  const data = await axios(url);
+  const {data} = await axios(url);
   console.log(data);
+  // setTutorials(data);
 
 
 };
+useEffect(() => {
+  getTutorials();
+ 
+  }, [])
 
-getTutorials()
+
 
   return (
     <>
